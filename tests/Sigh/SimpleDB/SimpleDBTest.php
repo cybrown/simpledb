5a53b2c7464918ecbe4fd20fc3ef2a05f3a66096
Cy Brown <cy@cybrown.fr>
2013-07-15 14:15:49 +0200

    Added one dummy test case for future tests

---
+++ tests/Sigh/SimpleDB/SimpleDBTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sigh\SimpleDB\Test;
+
+class SimpleDBTest extends \PHPUnit_Framework_TestCase {
+    
+    public function testTrue () {
+        $this->assertTrue(true);
+    }
+    
+}
